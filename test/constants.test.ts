1257b452e4230de3b7c8b2baace7b83cc6287979
AFXMaster <tempusresearch1@gmail.com>
2021-01-04 23:12:49 +0000

    Changes test to BSC

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -1,16 +1,16 @@
 import { INIT_CODE_HASH } from '../src/constants'
 
-import { bytecode } from '@uniswap/v2-core/build/UniswapV2Pair.json'
-import { keccak256 } from '@ethersproject/solidity'
+//import { bytecode } from '@uniswap/v2-core/build/UniswapV2Pair.json'
+//import { keccak256 } from '@ethersproject/solidity'
 
 // this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
 // and load the JSON.
-const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
+//const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
 
 describe('constants', () => {
   describe('INIT_CODE_HASH', () => {
     it('matches computed bytecode hash', () => {
-      expect(COMPUTED_INIT_CODE_HASH).toEqual(INIT_CODE_HASH)
+      expect(INIT_CODE_HASH).toEqual(0x4101f5607d53242625fa4dd8e6b435b990b434c7511a4a6b808972b114d2c60f)
     })
   })
 })
